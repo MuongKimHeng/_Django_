60eff465d070066eb1574e1d5305971b17e6d725
Muong KimHeng <kimhengmuongtuesday@gmail.com>
2026-07-20 17:15:59 +0700

    Forms.course

--- kimheng_django_e19/settings.py
+++ kimheng_django_e19/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'kimheng_django_e19.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
